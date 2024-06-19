b07eaa375aad5de93eabcd559561c3306c2807b9
Camilo Ordoñez <cronox20@gmail.com>
2024-06-19 10:54:57 -0400

    Agregar archivos de proyecto.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using LabWebApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LabWebApp.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public IConfiguration _config { get; set; }
+        public ApplicationDbContext(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(_config.GetConnectionString("DatabaseConnection"));
+        }
+
+        public DbSet<Product> Products;
+    }
+}
